c7a58caffa45d62cc9f2ae47963242e30304c115
HirasParasian <hirasparasian@gmail.com>
2022-03-22 21:53:03 +0700

    npm init

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/application.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@popperjs/core'
 
 function MyApp({ Component, pageProps }) {
 
