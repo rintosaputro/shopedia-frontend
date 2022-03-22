d5e79f4cb417eb486b07a09548d90bf3f631f923
HirasParasian <hirasparasian@gmail.com>
2022-03-23 01:05:07 +0700

    Feat : Page Login

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,16 @@ import '../styles/globals.css'
 import '../styles/application.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@popperjs/core'
+import { Provider } from 'react-redux';
+import store from '../redux/store'
 
 function MyApp({ Component, pageProps }) {
 
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
