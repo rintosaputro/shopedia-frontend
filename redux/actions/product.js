97277aae76c5399a449637c0caad65e56e1100f8
HirasParasian <hirasparasian@gmail.com>
2022-03-25 16:33:57 +0700

    Feat : Home update, fix error handling and add Coming soon

--- redux/actions/product.js
+++ redux/actions/product.js
@@ -1,16 +1,26 @@
 import http from "../../helper/http";
 
 export const getProduct = async (dispatch) => {
-  dispatch({
-    type: 'TOGGLE_LOADING'
-  })
-  const token = window.localStorage.getItem('token')
-  const { data } = await http(token).get('http://localhost:3000/products?sort=DESC&orderBy=id')
-  dispatch({
-    type: 'GET_PRODUCT',
-    payload: data.results
-  })
-  dispatch({
-    type: 'TOGGLE_LOADING'
-  })
+  try {
+    dispatch({
+      type: 'TOGGLE_LOADING'
+    })
+    const token = window.localStorage.getItem('token')
+    const { data } = await http(token).get('http://localhost:3000/products?sort=DESC&orderBy=id')
+    dispatch({
+      type: 'GET_PRODUCT',
+      payload: data.results
+    })
+    dispatch({
+      type: 'TOGGLE_LOADING'
+    })
+  } catch (err) {
+    dispatch({
+      type: 'PRODUCT_ERROR',
+      payload: err.response.data
+    })
+    dispatch({
+      type: 'TOGGLE_LOADING'
+    })
+  }
 }
