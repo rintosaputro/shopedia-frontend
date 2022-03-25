33c73c829d0dee5f795866566646ae1d3830ced9
rintosaputro <saputrorinto@gmail.com>
2022-03-25 22:20:54 +0700

    feat: integration to backend

--- pages/selling-product/index.js
+++ pages/selling-product/index.js
@@ -11,42 +11,109 @@ import { useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
 import styles from './Selling.module.css'
 import noImg from '../../images/noImg.jpg'
+import http from '../../helper/http'
+import { Alert, Spinner } from 'react-bootstrap'
+import { useRouter } from 'next/router'
 
 const Index = () => {
+  const [brand, setBrand] = useState([])
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [err, setErr] = useState(false)
+  const [message, setMessage] = useState('')
+  const [success, setSuccess] = useState(false)
+
+  const route = useRouter();
+
   const { auth } = useSelector(state => state);
 
   const [img, setImg] = useState();
 
+  const getBrand = async () => {
+    const { data } = await http().get('/brands')
+    setBrand([...data.results])
+  }
+  const getCategory = async () => {
+    const { data } = await http().get('/categories')
+    setCategories(data.results)
+  }
+
   useEffect(() => {
-    console.log(auth)
+    getBrand();
+    getCategory();
   }, [])
   
   const imageChange = (event) => {
     if (event.target.files && event.target.files[0]) {
       setImg(URL.createObjectURL(event.target.files[0]));
     }
-    // const selectedFIles =[];
-    // const targetFiles =event.target.files;
-    // const targetFilesObject= [...targetFiles]
-    // targetFilesObject.map((file)=>{
-    //     return selectedFIles.push(URL.createObjectURL(file))
-    // })
-    // setImg(selectedFIles);
   }
 
-  const sellProduct = (e) => {
+  const sellProduct = async (e) => {
     e.preventDefault();
+    setLoading(true)
+    setErr(false);
     const goods = document.getElementById('goods').value
     const description = document.getElementById('description').value
     const price = document.getElementById('price').value
     const stock = document.getElementById('stock').value
-    const condition = document.querySelector('.form-check-input:checked');
+    const condition = document.querySelector('.stockRadio .form-check-input:checked');
+    const brand = document.querySelector('.brandRadio .form-check-input:checked');
+    const category = document.querySelector('.categoriesRadio .form-check-input:checked');
     const file = document.getElementById('fileImg').files[0]
     let resCondition;
-    if (condition) {
+    let resBrand;
+    let resCategory;
+    if (condition && category) {
       resCondition = condition.value
+      resCategory = category.value
     }
-    console.log(goods, description, price, stock, resCondition, file); 
+    if (brand) {
+      resBrand = brand.value;
+    }
+
+    const param = new URLSearchParams();
+    param.append('name', goods)
+    param.append('description', description)
+    param.append('price', price)
+    param.append('stock', stock)
+    param.append('condition', resCondition)
+    param.append('brandId', resBrand)
+    param.append('categoryId', resCategory)
+
+    const token = window.localStorage.getItem('token')
+
+    if (file) {
+      await http(token).post('/products', param)
+      .then(async (res) => {
+        if (res.status < 400) {
+          console.log('res', res)
+          const files = new FormData();
+          files.append('image', file)
+          files.append('productId', res.data.results.id)
+          await http(token).post('/products/image', files)
+          .then(res => {
+            console.log('image', res.data.results)
+          })
+          .catch(err => {
+            console.log('image', err.response.data.message)
+          })
+          setMessage(res.data.results.message)
+          setSuccess(true)
+          setLoading(false);
+          route.push('/my-product')
+        }
+      }).catch(err => {
+        setLoading(false);
+        setErr(true);
+        console.log(err.response.data.message)
+        setMessage(err.response.data.message)
+      })
+    } else {
+      setErr(true);
+      setMessage('Please add your image products')
+    }
+    setLoading(false)
   }
 
   return (
@@ -123,7 +190,8 @@ const Index = () => {
                   inline
                   label="New Product"
                   name="group1"
-                  value={1}
+                  className='stockRadio'
+                  value='New'
                 />
               </Col>
               <Col>
@@ -131,9 +199,38 @@ const Index = () => {
                   inline
                   label="Second Product"
                   name="group1"
-                  value={2}
+                  className='stockRadio'
+                  value='Second'
                 />
               </Col>
+            </Row>
+            <Row className='mt-5 g-1'>
+              <div className='mb-3'>Brands</div>
+              {brand.map((data, index) => {
+                return <Col key={data.name} xs={6} lg={4}>
+                  <Form.Check
+                  inline
+                  label={data.name}
+                  name="group1"
+                  className='brandRadio'
+                  value={data.id}
+                />
+                </Col>
+              })}
+            </Row>
+            <Row className='mt-5 g-1'>
+              <div className='mb-3'>Categories</div>
+              {categories.map((data, index) => {
+                return <Col key={data.name} xs={6} lg={4}>
+                  <Form.Check
+                  inline
+                  label={data.name}
+                  name="group1"
+                  className='categoriesRadio'
+                  value={data.id}
+                />
+                </Col>
+              })}
             </Row>
             <h4 className='mt-5'>Photo of Goods</h4>
             <Row className='my-5'>
@@ -168,9 +265,12 @@ const Index = () => {
 
               </Col>
             </Row>
+            {err && <Alert variant='color1' className='text-danger text-center mt-5'>{message}</Alert>}
+            {success && <Alert variant='color1' className='text-center mt-5'>Successfully added new product!</Alert>}
             <Button onClick={sellProduct} className={`mt-4 px-4 ${styles.btnSell}`} variant="color2" size="lg" active>
-              Sell Product
-            </Button>{' '}
+              {loading ? <Spinner animation="border" /> : 'Sell Product'}
+            </Button>
+            {' '}
           </Col>
           <Col xs={12} md={3}>
 
