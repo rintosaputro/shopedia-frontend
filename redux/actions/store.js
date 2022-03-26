259b1aae951809952895b6e751623bc15b499b9a
HirasParasian <hirasparasian@gmail.com>
2022-03-26 22:55:34 +0700

    Feat : My Store

--- redux/actions/store.js
+++ redux/actions/store.js
@@ -30,3 +30,17 @@ export const createStore = (name, description) => {
   }
 }
 
+export const getStore = async (dispatch) => {
+  dispatch({
+    type: 'TOGGLE_LOADING'
+  })
+  const token = window.localStorage.getItem('token')
+  const { data } = await http(token).get('http://localhost:3000/users/profile')
+  dispatch({
+    type: 'GET_PROFILE',
+    payload: data.results
+  })
+  dispatch({
+    type: 'TOGGLE_LOADING'
+  })
+}
