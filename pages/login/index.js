794b04493f6884c7da52c094a3a43d1eef4a3a74
HirasParasian <hirasparasian@gmail.com>
2022-03-27 21:09:42 +0700

    Fix : Login Logout

--- pages/login/index.js
+++ pages/login/index.js
@@ -31,24 +31,25 @@ const Login = () => {
     const email = document.getElementById('email').value;
     const pwd = document.getElementById('password').value;
     dispatch(login(email, pwd));
+    route.reload()
   }
 
 
   return (
     <>
-    <Layout>
-      <Head>
-        <title>Login | Shopedia</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <div className={'bg-color4 py-5'}>
-        <h1 className={'text-center pt-5'}>My Account</h1>
-        <div className={'text-center pb-5'}>Register and log in with your account to be able to shop at will</div>
-      </div>
-      <Container>
-        <Row className='py-5'>
+      <Layout>
+        <Head>
+          <title>Login | Shopedia</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <div className={'bg-color4 py-5'}>
+          <h1 className={'text-center pt-5'}>My Account</h1>
+          <div className={'text-center pb-5'}>Register and log in with your account to be able to shop at will</div>
+        </div>
+        <Container>
+          <Row className='py-5'>
             <Col xs={12} md={4}>
-                <Sidebar />
+              <Sidebar />
             </Col>
             <Col xs={12} md={5}>
               <div className='my-5'>
@@ -63,37 +64,37 @@ const Login = () => {
                   placeholder='User name or email address'
                 />
                 <Form.Control
-                    type="password"
-                    id="password"
-                    name="password"
-                    aria-describedby="password"
-                    className='me-5 py-3 mt-2'
-                    placeholder='password'
+                  type="password"
+                  id="password"
+                  name="password"
+                  aria-describedby="password"
+                  className='me-5 py-3 mt-2'
+                  placeholder='password'
                 />
                 <Button onClick={handleSubmit} className='mt-4 px-4' variant="color2" size="lg" active>
-                    Login
+                  Login
                 </Button>{' '}<br />
                 <Row className='mt-3'>
-                    <Col>
-                        <Form.Check
-                            inline
-                            label="Remember Me"
-                            name="group1"
-                        />
-                    </Col>
-                    <Col>
-                        <Link href='/forgot-password'>
-                            <a className='text-decoration-none text-color3'>Forgot your password?</a>
-                        </Link>
-                    </Col>
+                  <Col>
+                    <Form.Check
+                      inline
+                      label="Remember Me"
+                      name="group1"
+                    />
+                  </Col>
+                  <Col>
+                    <Link href='/forgot-password'>
+                      <a className='text-decoration-none text-color3'>Forgot your password?</a>
+                    </Link>
+                  </Col>
                 </Row>
               </div>
             </Col>
             <Col xs={12} md={3}>
             </Col>
           </Row>
-      </Container>
-    </Layout>
+        </Container>
+      </Layout>
     </>
   )
 }
